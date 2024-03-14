5e2f7ef4d44bfa36daa0a3993e068a8307dd3ff3
Ansh Jindal <anshjindal132@gmail,com>
2024-03-14 16:34:06 +0530

    fixed bugs in making server

--- backend1/server.js
+++ backend1/server.js
@@ -1,6 +1,6 @@
 const app =require("./app")
 const dotenv=require("dotenv")
-dotenv.config({path:"backend/config/config.env"})
+dotenv.config({path:"backend1/config/config.env"})
 app.listen(process.env.PORT,()=>{
     console.log("server is working on port http://localhost:", process.env.PORT);
 })
