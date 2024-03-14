1885220f852b03553932273a6483cd58cc233146
Ansh Jindal <anshjindal132@gmail,com>
2024-03-14 17:02:29 +0530

    connected database

--- backend1/server.js
+++ backend1/server.js
@@ -1,6 +1,11 @@
 const app =require("./app")
 const dotenv=require("dotenv")
+const connectDatabase=require("./config/database.js")
 dotenv.config({path:"backend1/config/config.env"})
+//call connect database after dotenv as usko fir peocess.env milega ni
+connectDatabase();
 app.listen(process.env.PORT,()=>{
     console.log("server is working on port http://localhost:", process.env.PORT);
 })
+
+//to run write npm run dev  it will automatically do nodemon server.js
